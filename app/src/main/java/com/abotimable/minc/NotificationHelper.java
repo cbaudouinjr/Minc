600d0cce88a06260ba63127e34ea16f36bf0183d
Elliot Miller <elliot@bitoffdev.com>
2019-10-06 07:03:47 -0400

    Add firebase-messaging dependency and requisite small icon

--- app/src/main/java/com/abotimable/minc/NotificationHelper.java
+++ app/src/main/java/com/abotimable/minc/NotificationHelper.java
@@ -9,6 +9,7 @@ public class NotificationHelper {
 
     public static void displayNotification(Context context, String title, String body){
         NotificationCompat.Builder nBuilder = new NotificationCompat.Builder(context, MainActivity.CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle(title)
                 .setContentText(body)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
