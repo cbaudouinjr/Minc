9f61900a9169585f2cc1dc913839a6ff1c6441f9
Elliot Miller <elliot@bitoffdev.com>
2019-10-06 11:41:56 -0400

    Use a project image for the notification icon

--- app/src/main/java/com/abotimable/minc/NotificationHelper.java
+++ app/src/main/java/com/abotimable/minc/NotificationHelper.java
@@ -8,8 +8,11 @@ import androidx.core.app.NotificationManagerCompat;
 public class NotificationHelper {
 
     public static void displayNotification(Context context, String title, String body){
+        // WARNING: You need to include a small icon, or some Android version will throw errors.
+        // Furthermore, you should use an image located in the project heirarchy, not a system
+        // image. Using system images seems to cause errors after a period of time.
         NotificationCompat.Builder nBuilder = new NotificationCompat.Builder(context, MainActivity.CHANNEL_ID)
-                .setSmallIcon(R.drawable.common_full_open_on_phone)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
                 .setContentTitle(title)
                 .setContentText(body)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
